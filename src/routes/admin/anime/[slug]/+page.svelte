7d37050d77de8761890f227d2b384130a9fc8d07
Gareeb Vi <61916927+kai00588@users.noreply.github.com>
2024-01-18 11:39:45 +0000

    content anime page

--- src/routes/admin/anime/[slug]/+page.svelte
+++ src/routes/admin/anime/[slug]/+page.svelte
@@ -3,5 +3,14 @@ export let data;
 const {anime} = data;
 </script>
 <div>
-<h2>{anime?.title}</h2>
+    <div class="grid grid-cols-2">
+        <div class="w-full p-2">
+            <img class="" src="{anime?.image}" alt="{anime?.title}" />
+        </div>
+        <div class="w-full p-2">
+            <h1>{anime?.title}</h1>
+            <div>
+            </div>
+        </div>
+    </div>
 </div>
