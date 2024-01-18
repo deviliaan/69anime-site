1847f25fa3a1fc01282304701b4aa8649c3d680f
Gareeb Vi <61916927+kai00588@users.noreply.github.com>
2024-01-18 12:18:42 +0000

    admin content page added

--- src/routes/admin/anime/[slug]/+page.svelte
+++ src/routes/admin/anime/[slug]/+page.svelte
@@ -7,10 +7,26 @@ const {anime} = data;
         <div class="w-full p-2">
             <img class="" src="{anime?.image}" alt="{anime?.title}" />
         </div>
-        <div class="w-full p-2">
-            <h1>{anime?.title}</h1>
-            <div>
+        <div class="w-full p-2 flex flex-col flex-wrap">
+            <div class="stats shadow">
+                <div class="stat p-2">
+                    <div class="stat-title">{anime?.title}</div>
+                    <p class="text-sm">Mal Id: {anime?.malID}</p>
+                    <p class="text-sm">Episodes: {anime?.totalEpisodes}</p>
+                    <p class="text-sm">Type: {anime?.type}</p>
+                </div>
             </div>
+            <div class="stats shadow">
+                <div class="stat p-2">
+                    <button class="btn text-accent">Add</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="stats shadow">
+        <div class="stat p-2">
+            <p class="stat-title text-accent">Synopsis</p>
+            <p class="text-sm">{anime?.description}</p>
         </div>
     </div>
 </div>
