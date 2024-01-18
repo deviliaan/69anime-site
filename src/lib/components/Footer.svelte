fee5b409349d813f8877f707be861bf6f0cdd9b1
Gareeb Vi <61916927+kai00588@users.noreply.github.com>
2024-01-18 12:57:41 +0000

    tabler added

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+import {IconHeart} from '@tabler/icons-svelte';
+</script>
+<footer class="footer items-center p-4 bg-neutral text-neutral-content">
+    <aside class="items-center p-2 grid-flow-col">
+        <p class="text-sm">© 2024 - All right reserved</p>
+    </aside>
+    <nav class="grid-flow-col gap-4 md:place-self-center p-2 md:justify-self-end">
+        <IconHeart />
+    </nav>
+</footer>
--- src/routes/admin/anime/[slug]/+layout.svelte
+++ src/routes/admin/anime/[slug]/+layout.svelte
@@ -1,8 +1,11 @@
 <script>
 import Navbar from '$lib/components/Navbar.svelte';
+import Footer from '$lib/components/Footer.svelte';
+
 </script>
 
 <div>
 <Navbar />
     <slot />
+<Footer />
 </div>
