a5bb7dbe8a61aac10b4c558e0ea43da3f2f90b2b
Gareeb Vi <61916927+kai00588@users.noreply.github.com>
2024-01-18 11:22:23 +0000

    admin recent added

--- src/routes/admin/recent/+page.svelte
+++ src/routes/admin/recent/+page.svelte
@@ -1,11 +1,13 @@
 <script>
 export let data;
+import Card from '$lib/components/Card.svelte';
 const {results} = data;
 </script>
 
-<div>
-  <h2>Admin Page</h2>
-  {#each results as anime}
-    <h2>{anime.title}</h2>
-  {/each}
+<div class="w-full">
+  <div class="grid w-full grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-2 gap-y-2 p-2">
+    {#each results as anime}
+      <Card anime={anime} />
+    {/each}
+  </div>
 </div>
