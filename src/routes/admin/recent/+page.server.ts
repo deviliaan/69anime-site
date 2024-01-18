7da1b1ec1ab57d1cfecf8f073f79822bae182003
Gareeb Vi <61916927+kai00588@users.noreply.github.com>
2024-01-18 10:29:10 +0000

    Admin page added

--- src/routes/admin/recent/+page.server.ts
+++ src/routes/admin/recent/+page.server.ts
@@ -1,7 +1,11 @@
+import {provider} from '$lib/provider'
 export async function load() {
+    const res = await provider.fetchRecentEpisodes().then((data)=>{
+        return data
+    })
     return {
-        props: {
-            title: 'Home'
-        }
+        currentPage: res.currentPage,
+        hasNextPage: res.hasNextPage,
+        results: res.results
     }
 }
--- src/routes/admin/recent/+page.svelte
+++ src/routes/admin/recent/+page.svelte
@@ -1,3 +1,11 @@
+<script>
+export let data;
+const {results} = data;
+</script>
+
 <div>
   <h2>Admin Page</h2>
+  {#each results as anime}
+    <h2>{anime.title}</h2>
+  {/each}
 </div>
